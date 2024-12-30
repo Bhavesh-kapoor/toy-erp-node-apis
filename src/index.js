4d26eb9d8ee0f29f0c87929a8eb291e32d73a64a
Deepak Singh <deepaky954000@gmail.com>
2024-12-30 18:01:12 +0530

    minor linting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import app from "#configs/server";
 import connectDB from "#configs/database";
 import env from "#configs/env";
-import User from "#models/user"
-
+import User from "#models/user";
 
 await connectDB(env.DB_URI);
 
-
-app.listen(env.PORT,()=>{
-    console.log(`Server is running at ${env.PORT}`);
+app.listen(env.PORT, () => {
+  console.log(`Server is running at ${env.PORT}`);
 });
-
--- src/models/role.model.js
+++ src/models/role.model.js
@@ -1,19 +1,13 @@
-
 import mongoose from "mongoose";
 
-const RoleSchema = new mongoose.Schema(
-    {
-     name: {
-        type:  String,
-        required :  true,
-        trim  : true
-     },
-     permission:{
-        type : mongoose.Schema.Types.Mixed
-     }
-    
-
-     
-    }
-)
-export default  mongoose.model("Role" , RoleSchema);
+const RoleSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  permission: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+});
+export default mongoose.model("Role", RoleSchema);
