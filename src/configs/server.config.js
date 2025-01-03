b6344c372dd88c49968ca4fa44dae55edea25d23
Rishabh gupta <guptarishabh792@gmail.com>
2025-01-03 15:34:03 +0530

    Add winston , colors ,  figlet

--- src/configs/server.config.js
+++ src/configs/server.config.js
@@ -1,5 +1,7 @@
 import multer from "multer";
+import colors from "colors";
 import express from "express";
+import { logger } from "./logger.js";
 import routeMapper from "#routes/index";
 import globalErrorHandler from "#utils/error";
 import { authentication } from "#middlewares/auth";
@@ -7,6 +9,29 @@ import queryHandler from "#middlewares/queryHandler";
 import sessionMiddleware from "#middlewares/session";
 
 const app = express();
+
+app.use((req, res, next) => {
+  const startTime = process.hrtime();
+  res.on("finish", () => {
+    const fetchStatus = () => {
+      if (res.statusCode >= 500) return colors.red(`${res.statusCode}`);
+      else if (res.statusCode >= 400) return colors.yellow(`${res.statusCode}`);
+      else if (res.statusCode >= 300) return colors.cyan(`${res.statusCode}`);
+      else if (res.statusCode >= 200) return colors.green(`${res.statusCode}`);
+      else return colors.white(`${res.statusCode}`);
+    };
+    const diff = process.hrtime(startTime);
+    const responseTime = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(2);
+    logger.info(
+      `${"METHOD:".blue} ${req.method.yellow} - ${"URL:".blue} ${
+        req.originalUrl.yellow
+      } - ${"STATUS:".blue} ${fetchStatus()} - ${"Response Time:".blue} ${
+        responseTime.magenta
+      } ${"ms".magenta}`
+    );
+  });
+  next();
+});
 
 app.use(multer().any());
 app.use(express.json());
