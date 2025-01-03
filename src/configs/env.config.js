d4c47e2e45bd74bf47beb7a91541f48c64c2699c
Deepak Singh <deepaky954000@gmail.com>
2025-01-03 16:13:09 +0530

    added brand validation schema

--- src/configs/env.config.js
+++ src/configs/env.config.js
@@ -6,6 +6,6 @@ const env = cleanEnv(process.env, {
   PORT: num({ default: 8000 }),
   DB_URI: str(),
   JWT_SECRET: str(),
-  JWT_TOKEN_AGE: str("30d"),
+  JWT_TOKEN_AGE: str({ default: "30d" }),
 });
 export default env;
