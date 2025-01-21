698362ee2b5894e286cdd32abf6b3a91519fb9e6
Deepak Singh <deepaky954000@gmail.com>
2025-01-21 12:31:09 +0530

    removed pincode

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,7 +8,6 @@ import uploadFile from "#utils/uploadFile";
 export const addressSchema = new Schema({
   line1: {
     type: String,
-    required: true,
     trim: true,
   },
   street: {
@@ -17,26 +16,18 @@ export const addressSchema = new Schema({
   },
   city: {
     type: String,
-    required: true,
     trim: true,
   },
   state: {
     type: String,
-    required: true,
     trim: true,
   },
   pinCode: {
     type: String,
-    required: true,
     trim: true,
-    validate: {
-      validator: (value) => /^[1-9]\d{4,9}$/.test(value), // Matches a 10-digit PIN code
-      message: "Please enter a valid PIN code.",
-    },
   },
   country: {
     type: String,
-    required: true,
     trim: true,
   },
   landmark: {
