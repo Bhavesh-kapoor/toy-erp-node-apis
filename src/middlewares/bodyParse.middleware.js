f7345afefbfa1be9c6ce91516805707333c55acd
coolboyalan <deepaky954000@gmail.com>
2025-02-28 15:06:08 +0530

    fixed query parser

--- src/middlewares/bodyParse.middleware.js
+++ src/middlewares/bodyParse.middleware.js
@@ -3,6 +3,10 @@ import objectParser from "#utils/objectParser";
 
 const bodyParser = asyncHandler((req, _res, next) => {
   req.query = objectParser(req.query);
+  for (const key in (req, query)) {
+    if (typeof req.query[key] === "number")
+      req.query[key] = req.query[key].toString();
+  }
   if (req.method === "GET" || req.method === "DELETE") return next();
   if (
     !req.headers["content-type"] ||
